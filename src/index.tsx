ef4a499e72b186c5b00aeba1e65b471eab93172a
Ankit Kumar <ankitdayal1992@gmail.com>
2022-02-21 16:47:18 +0530

    Added Microsoft Fabric UI

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { initializeIcons } from '@fluentui/font-icons-mdl2';
+initializeIcons();
 
 ReactDOM.render(
   <React.StrictMode>
